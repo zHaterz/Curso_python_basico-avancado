5360de62b5631a5d816f4badfadc77cd18ee1392
Anderson <77762452+zHaterz@users.noreply.github.com>
2023-05-13 08:51:26 -0300

    Tabuda usando for é while, para sair do tedio.

---
+++ Mod_2/Tabela_mult.py
@@ -0,0 +1,15 @@
+
+print('Tabuada dos numeros:')
+while True:
+    entrada = input('Digite um numero ou [S] para sair: ')
+    
+    if entrada[0].lower() == 's':
+        break
+    
+    try:
+        if entrada:
+            for i in range(1,11):
+                resultado = int(entrada) * i 
+                print(f'{entrada} x {i} | {resultado}')
+    except:
+        print('Algo deu errado meu mano!\n começa de novo ai.')
